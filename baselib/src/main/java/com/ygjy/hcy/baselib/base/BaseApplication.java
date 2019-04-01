1a3def805d22d45f0c27ca91fa174d4afd19bb15
springyang <384317693@qq.com>
2019-04-01 17:23:57 +0800

    添加baseApplication

---
+++ baselib/src/main/java/com/ygjy/hcy/baselib/base/BaseApplication.java
@@ -0,0 +1,28 @@
+package com.ygjy.hcy.baselib.base;
+
+import android.app.Application;
+
+import com.orhanobut.logger.AndroidLogAdapter;
+import com.orhanobut.logger.Logger;
+import com.orhanobut.logger.PrettyFormatStrategy;
+
+/**
+ * @创建人：hcy
+ * @创建时间：2019/4/1
+ * @作用描述：Function
+ * @email:384317693@qq.com
+ **/
+public class BaseApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        initLogger();
+    }
+
+    private void initLogger() {
+        //可选每个日志的全局标记。 默认PRETTY_LOGGER
+        //https://github.com/orhanobut/logger
+        PrettyFormatStrategy strategy = PrettyFormatStrategy.newBuilder().tag("hcy2019").build();
+        Logger.addLogAdapter(new AndroidLogAdapter(strategy));
+    }
+}
