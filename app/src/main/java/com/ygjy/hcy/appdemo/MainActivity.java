8372c33e3362ad5833081194d367daaf17680d24
springyang <384317693@qq.com>
2019-04-01 17:22:29 +0800

    添加logger第三方框架

--- app/src/main/java/com/ygjy/hcy/appdemo/MainActivity.java
+++ app/src/main/java/com/ygjy/hcy/appdemo/MainActivity.java
@@ -1,25 +1,33 @@
 package com.ygjy.hcy.appdemo;
 
 
-
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.FrameLayout;
 
+import com.orhanobut.logger.Logger;
+import com.ygjy.hcy.baselib.base.BaseActivity;
 import com.ygjy.hcy.baselib.utils.FragmentUtils;
 
 import butterknife.BindView;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends BaseActivity {
+    @BindView(R.id.fragment_layout)
+    FrameLayout fragment_layout;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-       // startActivity(new Intent(MainActivity.this, TestActivty.class));
-        Fragment[] fragments=new Fragment[]{new ModuleAFragment(),new ModuleBFragment(),new ModuleCFragment()};
-        FragmentUtils.showFragmentByPosition(getSupportFragmentManager(),0,fragments,R.id.fragment_layout);
+    protected int setLayoutId() {
+        return R.layout.activity_main;
 
     }
+
+    @Override
+    protected void init() {
+        Fragment[] fragments = new Fragment[]{new ModuleAFragment(), new ModuleBFragment(), new ModuleCFragment()};
+        FragmentUtils.showFragmentByPosition(getSupportFragmentManager(), 0, fragments, R.id.fragment_layout);
+
+    }
+
 }
--- app/src/main/java/com/ygjy/hcy/appdemo/application/MainApplication.java
+++ app/src/main/java/com/ygjy/hcy/appdemo/application/MainApplication.java
@@ -3,16 +3,19 @@ package com.ygjy.hcy.appdemo.application;
 import android.app.Application;
 import android.util.Log;
 
+import com.orhanobut.logger.Logger;
+import com.ygjy.hcy.baselib.base.BaseApplication;
+
 /**
  * @创建人：hcy
  * @创建时间：2019/4/1
  * @作用描述：Function
  * @email:384317693@qq.com
  **/
-public class MainApplication extends Application {
+public class MainApplication extends BaseApplication {
     @Override
     public void onCreate() {
         super.onCreate();
-        Log.d("MainApplication::", "onCreate: ");
+        Logger.t("小").d("MainApplication,%s","onCreate");
     }
 }
